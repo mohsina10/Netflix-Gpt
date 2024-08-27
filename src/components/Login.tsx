125a48559d6abc819a0610a17ab7fd992d05343f
mohsina_mulani10 <mohsina.mulani@equip9.com>
2024-08-27 23:40:44 +0530

    Validations are added

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -1,18 +1,25 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Header from './Header'
 import { useFormik } from 'formik';
+import {signUpSchema} from "../validations/index";
 const initialValues={
   name:"", 
   email:"", 
   password:""
 }
+
 const Login = () => {
-  const {values, errors, handleBlur, handleChange, handleSubmit}=useFormik({
+  const [isSignedIn,setIsSignedIn] =useState(false);
+  const {values, errors, touched,handleBlur, handleChange, handleSubmit}=useFormik({
     initialValues:initialValues, 
+    validationSchema:signUpSchema,
     onSubmit:(values) =>{
       console.log("Values from form", values);
     }
   })
+  const handleClick =()=>{
+    setIsSignedIn(!isSignedIn);
+  }
   return (
     <div>
       <Header />
@@ -22,8 +29,28 @@ const Login = () => {
           alt="logo"
         />
       </div>
-      <form className="w-3/12 absolute  p-12 bg-black my-36 mx-auto right-0 left-0 text-white" onSubmit={handleSubmit}>
-        <h1 className="font-bold text-3xl py-4">Sign In</h1>
+      <form
+        className="w-3/12 absolute  p-12 bg-black my-36 mx-auto right-0 left-0 text-white"
+        onSubmit={handleSubmit}
+      >
+        <h1 className="font-bold text-3xl py-4">
+          {!isSignedIn ? "Sign In" : "Sign Up"}
+        </h1>
+        {isSignedIn && (
+          <>
+            <input
+              type="name"
+              name="name"
+              id="name"
+              value={values.name}
+              onChange={handleChange}
+              onBlur={handleBlur}
+              placeholder="Full Name"
+              className="p-4 my-4 w-full bg-gray-700"
+            />
+            {errors.name && touched.name && <p>{errors.name}</p>}
+          </>
+        )}
         <input
           type="email"
           name="email"
@@ -34,6 +61,7 @@ const Login = () => {
           placeholder="Email Address"
           className="p-4 my-4 w-full bg-gray-700"
         />
+        {errors.email && touched.email && (<p>{errors.email}</p>)}
         <input
           type="password"
           name="password"
@@ -44,7 +72,13 @@ const Login = () => {
           placeholder="Password"
           className="p-4 my-4 w-full bg-gray-700"
         />
-        <button className="p-4 my-6 bg-red-700 w-full">Sign In</button>
+        {errors.password && touched.password && (<p>{errors.password}</p>)}
+        <button className="p-4 my-6 bg-red-700 w-full">
+          {!isSignedIn ? "Sign In" : "Sign Up"}
+        </button>
+        <p className="py-4" onClick={handleClick}>
+          New to Netflix Sign Up Now
+        </p>
       </form>
     </div>
   );
